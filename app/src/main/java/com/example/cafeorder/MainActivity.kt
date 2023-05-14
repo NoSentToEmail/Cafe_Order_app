0e14bd6a0d3d75975139bdf9f81355abb26af180
NoSentToEmail <saniamax20001@gmail.com>
2023-05-14 11:18:50 +0300

    второй экран, после кажатия кнопки заказа на первом экране

--- app/src/main/java/com/example/cafeorder/MainActivity.kt
+++ app/src/main/java/com/example/cafeorder/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.cafeorder
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -17,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button_order = findViewById(R.id.buttonCreatOrder)
+        button_order = findViewById(R.id.buttonCreateOrder)
         editTextName = findViewById(R.id.editTextTextName)
         editTextPasword = findViewById(R.id.editTextTextPassword)
 
